b26d52405e1ce4cb8342db2ded00b3d472a969ff
phillychi3 <55632143+phillychi3@users.noreply.github.com>
2021-07-03 01:16:01 +0800

    sec

--- lolbot/bot.py
+++ lolbot/bot.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 bot= commands.Bot(command_prefix="!")
+
 @bot.command()
 async def test(ctx):
-    await.send.ctx("great")
-bot.run('TOKEN')
+    await ctx.send("great")
+    
+    
+bot.run('ODYwNTY3MzAzODE2NjA5ODEy.YN9HwQ.OIxTOveMX_ISq2HoCeBBtKzT-SU')
